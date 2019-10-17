38ed8c9f9517bd3fd2b712e88b15d0f562521123
Arpit Shah <collegeimprovements@gmail.com>
2019-10-17 22:29:37 +0530

    adds setup script for symliking vscode

--- .config/vscode/User/setup_vscode_for_dotfiles_by_symlinking.sh
+++ .config/vscode/User/setup_vscode_for_dotfiles_by_symlinking.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rm -rf ~/Library/Application\ Support/Code/Userln -s ~/.config/vscode/User
+rm -rf ~/Library/Application\ Support/Code/User
 ln -s ~/.config/vscode/User ~/Library/Application\ Support/Code/User 
