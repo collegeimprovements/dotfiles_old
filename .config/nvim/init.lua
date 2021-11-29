fd7a0542eedb8f8b8ca8adb7e2738c93d591adfc
arpit <collegeimprovements@gmail.com>
2021-11-29 23:35:51 +0530

    vim test

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -494,15 +494,19 @@ require("gitsigns").setup()
 
 -- vim-test
 vim.api.nvim_set_keymap("n", "t<C-n>", "<Cmd>TestNearest<CR>", {silent = true, noremap = true})
-vim.api.nvim_set_keymap("n", "tn", "<Cmd>TestNearest<CR>", {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "tn",     "<Cmd>TestNearest<CR>", {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "<C-m>",     "<Cmd>TestNearest<CR>", {silent = true, noremap = true})
 
 vim.api.nvim_set_keymap("n", "t<C-f>", "<Cmd>TestFile<CR>", {silent = true, noremap = true})
-vim.api.nvim_set_keymap("n", "tf", "<Cmd>TestFile<CR>", {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "tf",     "<Cmd>TestFile<CR>", {silent = true, noremap = true})
 
 vim.api.nvim_set_keymap("n", "t<C-s>", "<Cmd>TestSuite<CR>", {silent = true, noremap = true})
 
 vim.api.nvim_set_keymap("n", "t<C-l>", "<Cmd>TestLast<CR>", {silent = true, noremap = true})
-vim.api.nvim_set_keymap("n", "tl", "<Cmd>TestLast<CR>", {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "tl",     "<Cmd>TestLast<CR>", {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "<c-x>",     "<Cmd>TestLast<CR>", {silent = true, noremap = true})
+
+vim.api.nvim_set_keymap("n", "t<C-v>", "<Cmd>TestVisit<CR>", {silent = true, noremap = true})
 
 -- LSP Kind
 require("lspkind").init()
