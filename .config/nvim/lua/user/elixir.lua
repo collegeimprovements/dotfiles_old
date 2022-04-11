1c7fccbd7cc836e595f1b11de73be15489da2889
Arpit shah <collegeimprovements@gmail.com>
2022-04-11 10:17:41 +0530

    formatting

--- .config/nvim/lua/user/elixir.lua
+++ .config/nvim/lua/user/elixir.lua
@@ -8,29 +8,29 @@
 local lsp_signature = require("lsp_signature")
 
 local on_attach = function(client, bufnr)
-	lsp_signature.on_attach()
+  lsp_signature.on_attach()
 
-	local opts = { noremap = true, silent = true }
+  local opts = { noremap = true, silent = true }
 
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>cr", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>cf", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>cd", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "df", "<cmd>lua vim.lsp.buf.formatting_seq_sync()<cr>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>cr", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>cf", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>cd", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "df", "<cmd>lua vim.lsp.buf.formatting_seq_sync()<cr>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
 
-	vim.keymap.set("n", "gr", ":References<cr>", opts)
-	vim.keymap.set("n", "g0", ":DocumentSymbols<cr>", opts)
-	vim.keymap.set("n", "gW", ":WorkspaceSymbols<cr>", opts)
-	vim.keymap.set("n", "<leader>d", ":Diagnostics<cr>", opts)
+  vim.keymap.set("n", "gr", ":References<cr>", opts)
+  vim.keymap.set("n", "g0", ":DocumentSymbols<cr>", opts)
+  vim.keymap.set("n", "gW", ":WorkspaceSymbols<cr>", opts)
+  vim.keymap.set("n", "<leader>d", ":Diagnostics<cr>", opts)
 end
 
 local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
@@ -42,23 +42,23 @@ local path_to_elixirls = vim.fn.expand("~/language-servers/elixir-ls/release/lan
 local lspconfig = require("lspconfig")
 
 lspconfig.elixirls.setup({
-	require("lsp_signature").on_attach(),
-	cmd = { path_to_elixirls },
-	on_attach = on_attach,
-	capabilities = capabilities,
-	flags = { debounce_text_changes = 150 },
-	settings = {
-		elixirLS = {
-			dialyzerEnabled = false,
-			fetchDeps = false,
-		},
-	},
+  require("lsp_signature").on_attach(),
+  cmd = { path_to_elixirls },
+  on_attach = on_attach,
+  capabilities = capabilities,
+  flags = { debounce_text_changes = 150 },
+  settings = {
+    elixirLS = {
+      dialyzerEnabled = false,
+      fetchDeps = false,
+    },
+  },
 })
 
 lspconfig.efm.setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-	filetypes = { "elixir" },
+  capabilities = capabilities,
+  on_attach = on_attach,
+  filetypes = { "elixir" },
 })
 
 --------------------------------------------------------------------------------------
@@ -66,14 +66,14 @@ lspconfig.efm.setup({
 -- Adds |> when we press <C-l>,
 -- Adds |> IO.inspect(label: "here") when we press <C-h>
 vim.api.nvim_exec(
-	[[
+  [[
 augroup elixirbindings
   autocmd! elixirbindings
   autocmd Filetype elixir imap <buffer> <silent> <C-l> \|>
   autocmd Filetype elixir imap <buffer> <silent> <C-h> \|> IO.inspect(label: "Here")
   " autocmd Filetype elixir imap <buffer> <silent> <C-m> %{}
 augroup end
-]],
-	false
+]] ,
+  false
 )
 --------------------------------------------------------------------------------------
