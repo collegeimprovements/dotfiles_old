67c115b9624623e7a71de44eb843a3d3d0057896
Arpit shah <collegeimprovements@gmail.com>
2022-04-11 10:34:55 +0530

    goto-preview keymaps

--- .config/nvim/lua/user/goto-preview.lua
+++ .config/nvim/lua/user/goto-preview.lua
@@ -1,3 +1,7 @@
 require("goto-preview").setup({
-  default_mappings = false, -- Bind default mappings
+  default_mappings = true, -- Bind default mappings
 })
+
+-- nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>
+-- nnoremap gpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>
+-- nnoremap gP <cmd>lua require('goto-preview').close_all_win()<CR>
