e020f88d6a35217ed070a21210c68ae268a47caf
Arpit shah <collegeimprovements@gmail.com>
2022-04-12 15:09:55 +0530

    nvim tree

--- .config/nvim/lua/user/nvim-tree.lua
+++ .config/nvim/lua/user/nvim-tree.lua
@@ -1,18 +1,16 @@
-require("nvim-tree").setup({
-	update_focused_file = {
-		-- enables the feature
-		enable = true,
-		-- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
-		-- only relevant when `update_focused_file.enable` is true
-		update_cwd = true,
-		-- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
-		-- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
-		ignore_list = {},
+-- nvim-tree
+vim.g.nvim_tree_respect_buf_cwd = 1
 
-		update_focused_file = {
-			enable = true,
-			update_cwd = true,
-			ignore_list = {},
-		},
-	},
+require("nvim-tree").setup({
+  update_cwd = true,
+  update_focused_file = {
+    -- enables the feature
+    enable = true,
+    -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
+    -- only relevant when `update_focused_file.enable` is true
+    update_cwd = true,
+    -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
+    -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
+    ignore_list = {},
+  },
 })
