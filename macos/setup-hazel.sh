0597875935b6b462dc048e0737557eed1e628516
Arpit Shah <collegeimprovements@gmail.com>
2022-08-23 10:46:59 +0530

    setup brew and hazel

---
+++ macos/setup-hazel.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir ~/Documents/Review
+
+mkdir ~/Documents/Review/pdfs
+mkdir ~/Documents/Review/zips
+mkdir ~/Documents/Review/images
+mkdir ~/Documents/Review/videos
+mkdir ~/Documents/Review/screenshots
+mkdir ~/Documents/Review/others
+
