f7bf781a8af5d1202e8abacee30b7611c0d8321d
Arpit Shah <collegeimprovements@gmail.com>
2023-02-15 02:10:20 +0530

    nvim continues

--- .config/nvim/lua/plugins/goto-preview.lua
+++ .config/nvim/lua/plugins/goto-preview.lua
@@ -1,10 +1,23 @@
 return {
-	{
-		"rmagatti/goto-preview",
-		config = function()
-			require("goto-preview").setup({
-				default_mappings = true, -- Bind default mappings
-			})
-		end,
-	},
+  {
+    "rmagatti/goto-preview",
+    config = function()
+      require("goto-preview").setup({
+        default_mappings = true, -- Bind default mappings
+      })
+      vim.keymap.set(
+        "n",
+        "gp",
+        "<cmd>lua require('goto-preview').goto_preview_definition()<CR>",
+        { noremap = true }
+      )
+    end,
+  },
 }
+
+-- nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>
+-- nnoremap gpt <cmd>lua require('goto-preview').goto_preview_type_definition()<CR>
+-- nnoremap gpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>
+-- nnoremap gP <cmd>lua require('goto-preview').close_all_win()<CR>
+-- nnoremap gpr <cmd>lua require('goto-preview').goto_preview_references()<CR>
+-- vim.keymap.set("n", "gp", "<cmd>lua require('goto-preview').goto_preview_definition()<CR>", {noremap=true})
---
+++ .config/nvim/lua/plugins/symobls-outline.lua
@@ -0,0 +1,8 @@
+return {
+	"simrat39/symbols-outline.nvim",
+	{
+		config = function()
+			require("symbols-outline").setup()
+		end,
+	},
+}
