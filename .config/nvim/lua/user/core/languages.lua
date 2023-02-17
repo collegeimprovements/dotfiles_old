fb412fa8966ba557a329437898cbc48168599dd9
Arpit Shah <collegeimprovements@gmail.com>
2023-02-17 14:13:01 +0530

    confi

--- .config/nvim/lua/user/core/languages.lua
+++ .config/nvim/lua/user/core/languages.lua
@@ -2,7 +2,7 @@
 -- Adds |> when we press <C-l>,
 -- Adds |> IO.inspect(label: "here") when we press <C-h>
 vim.api.nvim_exec(
-	[[
+  [[
 augroup elixirbindings
   autocmd! elixirbindings
   autocmd Filetype elixir imap <buffer> <silent> <C-l> \|>
@@ -10,13 +10,13 @@ augroup elixirbindings
   " autocmd Filetype elixir imap <buffer> <silent> <C-m> %{}
 augroup end
 ]],
-	false
+  false
 )
 
 -- Format on save and heex
 -- This is a hack where we set heex as html first and then as eelixir to make sure all LSPs are available.
-vim.api.nvim_command("autocmd BufNew,BufNewFile,BufRead *.heex set ft=html")
+-- vim.api.nvim_command("autocmd BufNew,BufNewFile,BufRead *.heex set ft=html")
 vim.api.nvim_command("autocmd BufNew,BufNewFile,BufRead *.heex set ft=eelixir")
 
-vim.api.nvim_command("autocmd BufWritePre *.lua lua vim.lsp.buf.format({ async = true })")
-vim.api.nvim_command("autocmd BufWritePre *.ex,*.exs,*.eex,*.heex lua vim.lsp.buf.format({ async = true })")
+vim.api.nvim_command("autocmd BufWritePre *.lua lua vim.lsp.buf.format()")
+vim.api.nvim_command("autocmd BufWritePre *.ex,*.exs,*.eex,*.heex lua vim.lsp.buf.format()")
