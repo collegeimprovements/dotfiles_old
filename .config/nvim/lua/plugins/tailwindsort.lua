a5ef7fb54290fe64834b30a22c7e077ca8c2990a
Arpit Shah <collegeimprovements@gmail.com>
2023-02-17 14:14:38 +0530

    shell for tailwindsort

---
+++ .config/nvim/lua/plugins/tailwindsort.lua
@@ -0,0 +1,12 @@
+return {
+  -- "laytan/tailwind-sorter.nvim",
+  -- dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-lua/plenary.nvim" },
+  -- build = "cd formatter && npm i && npm run build",
+  -- config = function()
+  -- 	require("tailwind-sorter").setup({
+  -- 		on_save_enabled = true, -- If `true`, automatically enables on save sorting.
+  -- 		on_save_pattern = { "*.heex", "*.html", "*.js", "*.jsx", "*.tsx", "*.twig", "*.hbs", "*.php" }, -- The file patterns to watch and sort.
+  -- 		node_path = "node",
+  -- 	})
+  -- end,
+}
