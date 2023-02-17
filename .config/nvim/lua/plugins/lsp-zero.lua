1fbc9f4c686bf1435fd9edc3801720e85754309d
Arpit Shah <collegeimprovements@gmail.com>
2023-02-17 14:12:11 +0530

    format

--- .config/nvim/lua/plugins/lsp-zero.lua
+++ .config/nvim/lua/plugins/lsp-zero.lua
@@ -1,97 +1,110 @@
 return {
-	{
-		"VonHeikemen/lsp-zero.nvim",
-		lazy = false,
-		-- event = "BufReadPre",
+  {
+    "VonHeikemen/lsp-zero.nvim",
+    lazy = false,
+    -- event = "BufReadPre",
 
-		dependencies = {
-			"neovim/nvim-lspconfig",
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-nvim-lua",
-			"hrsh7th/cmp-path",
-			"hrsh7th/nvim-cmp",
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-			{ "lukas-reineke/lsp-format.nvim", config = true },
-		},
-		config = function()
-			local lsp = require("lsp-zero")
-			local path_to_elixirls = vim.fn.expand("~/language-servers/elixir-ls/release/language_server.sh")
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-nvim-lua",
+      "hrsh7th/cmp-path",
+      "hrsh7th/nvim-cmp",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+      { "lukas-reineke/lsp-format.nvim", config = true },
+    },
+    config = function()
+      local lsp = require("lsp-zero")
+      local path_to_elixirls = vim.fn.expand("~/language-servers/elixir-ls/release/language_server.sh")
 
-			lsp.preset("recommended")
+      lsp.preset("recommended")
 
-			lsp.setup_nvim_cmp({
-				mapping = lsp.defaults.cmp_mappings,
-				sources = {
-					{ name = "path" },
-					{ name = "nvim_lsp", keyword_length = 1 },
-					{ name = "buffer", keyword_length = 3 },
-					{ name = "luasnip", keyword_length = 2 },
-				},
-			})
+      lsp.setup_nvim_cmp({
+        mapping = lsp.defaults.cmp_mappings,
+        sources = {
+          { name = "path" },
+          { name = "nvim_lsp", keyword_length = 1 },
+          { name = "buffer",   keyword_length = 3 },
+          { name = "luasnip",  keyword_length = 2 },
+        },
+      })
 
-			lsp.ensure_installed({
-				"lua_ls",
-				"emmet_ls",
-				"bashls",
-				"elixirls",
-				"efm",
-				"tsserver",
-				"eslint",
-				"tailwindcss",
-				"html",
-				"cssls",
-				"graphql",
-				"jsonls",
-			})
+      lsp.ensure_installed({
+        "lua_ls",
+        "emmet_ls",
+        "bashls",
+        "elixirls",
+        "efm",
+        "tsserver",
+        "eslint",
+        "tailwindcss",
+        "html",
+        "cssls",
+        "graphql",
+        "jsonls",
+      })
 
-			lsp.configure("elixirls", {
-				-- cmd = { path_to_elixirls },
-				flags = { debounce_text_changes = 150 },
-				settings = {
-					elixirLS = {
-						dialyzerEnabled = false,
-						fetchDeps = false,
-					},
-				},
-			})
+      lsp.configure("elixirls", {
+        -- cmd = { path_to_elixirls },
+        flags = { debounce_text_changes = 150 },
+        settings = {
+          elixirLS = {
+            dialyzerEnabled = false,
+            fetchDeps = false,
+          },
+        },
+      })
 
-			lsp.configure("efm", {
-				filetypes = { "elixir" },
-			})
+      lsp.configure("efm", {
+        filetypes = { "elixir" },
+      })
 
-			-- lua_ls Fix Undefined global 'vim'
-			lsp.configure("lua_ls", { settings = { Lua = { diagnostics = { globals = { "vim" } } } } })
+      -- lua_ls Fix Undefined global 'vim'
+      lsp.configure("lua_ls", { settings = { Lua = { diagnostics = { globals = { "vim" } } } } })
 
-			-- emmet_ls
-			lsp.configure("emmet_ls", {
-				filetypes = {
-					"html",
-					"heex",
-					"eelixir",
-					"eex",
-					"typescriptreact",
-					"javascriptreact",
-					"css",
-					"sass",
-					"scss",
-					"less",
-				},
-				init_options = {
-					html = {
-						options = {
-							-- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
-							["bem.enabled"] = true,
-						},
-					},
-				},
-			})
+      -- html
+      lsp.configure("html", {
+        filetypes = {
+          "html",
+          "heex",
+          "eelixir",
+          "eex",
+          "typescriptreact",
+          "javascriptreact",
+          "css",
+        },
+      })
 
-			lsp.setup()
-		end,
-	},
+      -- emmet_ls
+      lsp.configure("emmet_ls", {
+        filetypes = {
+          "html",
+          "heex",
+          "eelixir",
+          "eex",
+          "typescriptreact",
+          "javascriptreact",
+          "css",
+          "sass",
+          "scss",
+          "less",
+        },
+        init_options = {
+          html = {
+            options = {
+              -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+              ["bem.enabled"] = true,
+            },
+          },
+        },
+      })
+
+      lsp.setup()
+    end,
+  },
 }
--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -1,28 +1,28 @@
 return {
-	{
-		"jose-elias-alvarez/null-ls.nvim",
-		config = function()
-			local null_ls = require("null-ls")
-			local diagnostics = null_ls.builtins.diagnostics
-			local formatting = null_ls.builtins.formatting
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    config = function()
+      local null_ls = require("null-ls")
+      local diagnostics = null_ls.builtins.diagnostics
+      local formatting = null_ls.builtins.formatting
 
-			null_ls.setup({
-				debug = false,
-				sources = {
-					-- formatting.black.with({ extra_args = { "--fast" } }),
-					formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-					-- formatting.beautysh,
-					formatting.stylua,
-					formatting.mix,
-					formatting.jq,
-					formatting.pg_format,
-					-- formatting
-					diagnostics.credo,
-					diagnostics.eslint,
-					diagnostics.hadolint,
-					-- diagnostics.luacheck,
-				},
-			})
-		end,
-	},
+      null_ls.setup({
+        debug = false,
+        sources = {
+          -- formatting.black.with({ extra_args = { "--fast" } }),
+          formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+          -- formatting.beautysh,
+          formatting.stylua,
+          formatting.mix,
+          formatting.jq,
+          formatting.pg_format,
+          -- formatting
+          diagnostics.credo,
+          diagnostics.eslint,
+          diagnostics.hadolint,
+          -- diagnostics.luacheck,
+        },
+      })
+    end,
+  },
 }
