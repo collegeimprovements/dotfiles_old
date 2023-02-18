8daffd267e87aa553fcf8ef2e8251494a9a617f9
Arpit Shah <collegeimprovements@gmail.com>
2023-02-18 16:47:38 +0530

    luasnip

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -1,5 +1,8 @@
 return {
-  "L3MON4D3/LuaSnip",
-  -- install jsregexp (optional!).
-  build = "make install_jsregexp",
+	"L3MON4D3/LuaSnip",
+	-- install jsregexp (optional!).
+	build = "make install_jsregexp",
+	config = function()
+		require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/user/snippets/" })
+	end,
 }
