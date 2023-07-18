7d2934cbd1cfaaf12875f9e61b8f4e72964e6fcd
Arpit Shah <collegeimprovements@gmail.com>
2023-07-18 15:36:42 +0800

    updates lazy and nextls

--- .config/nvim/lua/plugins/nextls.lua
+++ .config/nvim/lua/plugins/nextls.lua
@@ -1,29 +1,31 @@
 return {
-	"elixir-tools/elixir-tools.nvim",
-	version = "*",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local elixir = require("elixir")
-		local elixirls = require("elixir.elixirls")
+  "elixir-tools/elixir-tools.nvim",
+  version = "*",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local elixir = require("elixir")
+    local elixirls = require("elixir.elixirls")
 
-		elixir.setup({
-			nextls = { enable = true },
-			credo = {},
-			elixirls = {
-				enable = true,
-				settings = elixirls.settings({
-					dialyzerEnabled = false,
-					enableTestLenses = false,
-				}),
-				on_attach = function(client, bufnr)
-					vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
-					vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
-					vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
-				end,
-			},
-		})
-	end,
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-	},
+    elixir.setup({
+      nextls = { enable = true },
+      credo = {},
+      elixirls = {
+        enable = true,
+        cmd = vim.fn.expand("~/language-servers/elixir-ls/release/language_server.sh"),
+        settings = elixirls.settings({
+          dialyzerEnabled = false,
+          fetchDeps = false,
+          enableTestLenses = false,
+        }),
+        on_attach = function(client, bufnr)
+          vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
+          vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
+          vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
+        end,
+      },
+    })
+  end,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
 }
