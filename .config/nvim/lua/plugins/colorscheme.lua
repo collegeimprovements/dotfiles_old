2000ca9cb6b829d7e49b02f4ab10686f855e9da2
Arpit Shah <collegeimprovements@gmail.com>
2023-07-18 10:20:38 +0800

    updates brewfile and nvim

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,16 @@
 -- colorscheme
 return {
-  {
-    "junegunn/seoul256.vim",
-    event = "VeryLazy",
+	{
+		"junegunn/seoul256.vim",
+		event = "VeryLazy",
 
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      vim.g.one_allow_italics = true
-      vim.g.seoul256_background = 234
-      -- load the colorscheme here
-      vim.cmd([[colorscheme seoul256]])
-    end,
-  },
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			vim.g.one_allow_italics = true
+			vim.g.seoul256_background = 234
+			-- load the colorscheme here
+			vim.cmd([[colorscheme seoul256]])
+		end,
+	},
 }
